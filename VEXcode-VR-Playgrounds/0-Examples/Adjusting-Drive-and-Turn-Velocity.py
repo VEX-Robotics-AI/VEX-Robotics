35172ba6b793300a17ce350248d7d43a02749713
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-10 19:49:24 -0700

    update example code

--- VEXcode-VR-Playgrounds/0-Examples/Adjusting-Drive-and-Turn-Velocity.py
+++ VEXcode-VR-Playgrounds/0-Examples/Adjusting-Drive-and-Turn-Velocity.py
@@ -5,26 +5,31 @@
 from vexcode import *
 
 
-def main():
-    # Set the turn velocity to 70%
-    drivetrain.set_turn_velocity(70, PERCENT)
+class VexRobot:
+    def main(self):
+        # Set the turn velocity to 70%
+        drivetrain.set_turn_velocity(70, PERCENT)
 
-    # Set the drive velocity to 45%
-    drivetrain.set_drive_velocity(45, PERCENT)
+        # Set the drive velocity to 45%
+        drivetrain.set_drive_velocity(45, PERCENT)
 
-    # Drive in a square moving forward
-    for repeat_count in range(4):
-        drivetrain.turn_for(RIGHT, 90, DEGREES)
-        drivetrain.drive_for(FORWARD, 300, MM)
-        # Brief wait needed at the end of loops for optimal performance
-        wait(5, MSEC)
+        # Drive in a square moving forward
+        for repeat_count in range(4):
+            drivetrain.turn_for(RIGHT, 90, DEGREES)
+            drivetrain.drive_for(FORWARD, 300, MM)
+            # Brief wait needed at the end of loops for optimal performance
+            wait(5, MSEC)
 
-    # Drive in a square moving in reverse
-    for repeat_count in range(4):
-        drivetrain.turn_for(RIGHT, 90, DEGREES)
-        drivetrain.drive_for(REVERSE, 300, MM)
-        # Brief wait needed at the end of loops for optimal performance
-        wait(5, MSEC)
+        # Drive in a square moving in reverse
+        for repeat_count in range(4):
+            drivetrain.turn_for(RIGHT, 90, DEGREES)
+            drivetrain.drive_for(REVERSE, 300, MM)
+            # Brief wait needed at the end of loops for optimal performance
+            wait(5, MSEC)
+
+        stop_project()
 
 
-vr_thread(main())
+VEX_ROBOT = VexRobot()
+
+vr_thread(VEX_ROBOT.main())
