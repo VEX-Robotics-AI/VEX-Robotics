1e36e4a5dc71de57cfce4702afa4243ad513bb1f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-25 21:29:41 -0800

    add autopilot classes for Drive-Forward-and-Reverse

---
+++ VEX-IQ/STEM-Labs/Drive-Forward-and-Reverse/autopilot_pyb.py
@@ -0,0 +1,26 @@
+from drivetrain import Drivetrain
+from vex import DistanceUnits, Motor, Ports
+
+
+class Autopilot:
+    def __init__(
+            self,
+            left_motor_port=Ports.PORT1, right_motor_port=Ports.PORT6,
+            wheel_travel=200, track_width=176,
+            distance_unit=DistanceUnits.MM,
+            gear_ratio=1):
+        self.drivetrain = \
+            Drivetrain(
+                Motor(
+                    left_motor_port,   # index
+                    False   # reverse
+                ),   # left_motor
+                Motor(
+                    right_motor_port,   # index
+                    True   # reverse
+                ),   # right_motor
+                wheel_travel,   # wheel_travel
+                track_width,   # track_width
+                distance_unit,   # distanceUnits
+                gear_ratio   # gear_ratio
+            )
