0aefa0f68f989d9685af74ee6de2fad562f6883a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-25 20:42:12 -0800

    add VEX-IQ Autopilot-Drive-Forward.PyB program

---
+++ VEX-IQ/STEM-Labs/Drive-Forward-and-Reverse/Autopilot-Drive-Forward.PyB.py
@@ -0,0 +1,38 @@
+from drivetrain import Drivetrain
+from vex import DirectionType, DistanceUnits, Motor, Ports, VelocityUnits
+
+
+class Autopilot:
+    def __init__(
+            self,
+            left_motor_port=Ports.PORT1, right_motor_port=Ports.PORT6,
+            wheel_travel=200, track_width=176,
+            distance_unit=DistanceUnits.MM,
+            gear_ratio=1):
+        self.drivetrain = \
+            Drivetrain(
+                Motor(
+                    left_motor_port,   # index
+                    False   # reverse
+                ),   # left_motor
+                Motor(
+                    right_motor_port,   # index
+                    True   # reverse
+                ),   # right_motor
+                wheel_travel,   # wheel_travel
+                track_width,   # track_width
+                distance_unit,   # distanceUnits
+                gear_ratio   # gear_ratio
+            )
+
+
+AUTOPILOT = Autopilot()
+
+AUTOPILOT.drivetrain.drive(
+    DirectionType.FWD,   # directionType
+    100,   # velocity
+    VelocityUnits.PCT   # velocityUnits
+)
+
+while True:
+    pass
