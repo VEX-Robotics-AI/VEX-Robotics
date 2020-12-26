5435d2b2cd769e38bb42e5d48f9118f973f48056
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-25 21:20:31 -0800

    add Autopilot-Drive-Forward and -Reverse Python programs

---
+++ VEX-IQ/STEM-Labs/Drive-Forward-and-Reverse/Autopilot-Drive-Reverse.PyA.py
@@ -0,0 +1,33 @@
+from drivetrain import Drivetrain
+from vexiq import Motor
+
+
+class Autopilot:
+    def __init__(
+            self,
+            left_motor_port=1, right_motor_port=6,
+            wheel_travel_mm=200, track_mm=176):
+        self.drivetrain = \
+            Drivetrain(
+                Motor(
+                    left_motor_port,   # port
+                    False   # switch_polarity
+                ),   # left_motor
+                Motor(
+                    right_motor_port,   # port
+                    True   # switch_polarity
+                ),   # right_motor
+                wheel_travel_mm,   # wheel_travel_mm
+                track_mm   # track_mm
+            )
+
+
+AUTOPILOT = Autopilot()
+
+AUTOPILOT.drivetrain.drive(
+    -100,   # power
+    None   # distance_mm
+)
+
+while True:
+    pass
