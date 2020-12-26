8cde34380226d00bf8eb61f7736a1ccd3c455dd5
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-26 00:03:45 -0800

    add VEX-IQ Autopilot-TouchLED-Color program

---
+++ VEX-IQ/STEM-Labs/Movement-Challenge/Autopilot-TouchLED-Color.py
@@ -0,0 +1,37 @@
+from drivetrain import Drivetrain
+from vex import ColorHue, DistanceUnits, Motor, Ports, Touchled
+
+
+class Autopilot:
+    def __init__(
+            self,
+            left_motor_port=Ports.PORT1, right_motor_port=Ports.PORT6,
+            wheel_travel=200, track_width=176,
+            distance_unit=DistanceUnits.MM,
+            gear_ratio=1,
+            touch_led_port=Ports.PORT2):
+        self.drivetrain = \
+            Drivetrain(
+                Motor(
+                    left_motor_port,   # index
+                    False   # reverse
+                ),   # left_motor
+                Motor(
+                    right_motor_port,   # index
+                    True   # reverse
+                ),   # right_motor
+                wheel_travel,   # wheel_travel
+                track_width,   # track_width
+                distance_unit,   # distanceUnits
+                gear_ratio   # gear_ratio
+            )
+
+        self.touch_led = Touchled(touch_led_port)
+
+
+AUTOPILOT = Autopilot()
+
+AUTOPILOT.touch_led.on_hue(
+    ColorHue.GREEN,   # color
+    100   # brightness
+)
