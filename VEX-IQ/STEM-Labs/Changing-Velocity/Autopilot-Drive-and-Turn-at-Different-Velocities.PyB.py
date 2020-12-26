ac34343464df0504878470d31391b633f582a1cf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-25 23:11:06 -0800

    add VEX-IQ Autopilot-Drive-and-Turn-at-Different-Velocities programs

---
+++ VEX-IQ/STEM-Labs/Changing-Velocity/Autopilot-Drive-and-Turn-at-Different-Velocities.PyB.py
@@ -0,0 +1,90 @@
+from drivetrain import Drivetrain
+from vex import \
+    DirectionType, \
+    DistanceUnits, \
+    Motor, \
+    Ports, \
+    RotationUnits, \
+    TurnType, \
+    VelocityUnits
+
+
+class Autopilot:
+    def __init__(
+            self,
+            left_motor_port=Ports.PORT1, right_motor_port=Ports.PORT6,
+            wheel_travel=200, track_width=176,
+            distance_unit=DistanceUnits.MM,
+            gear_ratio=1):
+        self.drivetrain = \
+            Drivetrain(
+                Motor(
+                    left_motor_port,   # index
+                    False   # reverse
+                ),   # left_motor
+                Motor(
+                    right_motor_port,   # index
+                    True   # reverse
+                ),   # right_motor
+                wheel_travel,   # wheel_travel
+                track_width,   # track_width
+                distance_unit,   # distanceUnits
+                gear_ratio   # gear_ratio
+            )
+
+
+AUTOPILOT = Autopilot()
+
+AUTOPILOT.drivetrain.drive_for(
+    DirectionType.FWD,   # directionType
+    5,   # distance
+    DistanceUnits.IN,   # distanceUnits
+    50,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
+
+AUTOPILOT.drivetrain.turn_for(
+    TurnType.RIGHT,   # turnType
+    90,   # angle
+    RotationUnits.DEG,   # rotationUnits
+    50,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
+
+AUTOPILOT.drivetrain.drive_for(
+    DirectionType.REV,   # directionType
+    5,   # distance
+    DistanceUnits.IN,   # distanceUnits
+    25,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
+
+AUTOPILOT.drivetrain.turn_for(
+    TurnType.LEFT,   # turnType
+    90,   # angle
+    RotationUnits.DEG,   # rotationUnits
+    75,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
+
+AUTOPILOT.drivetrain.drive_for(
+    DirectionType.FWD,   # directionType
+    5,   # distance
+    DistanceUnits.IN,   # distanceUnits
+    75,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
+
+AUTOPILOT.drivetrain.turn_for(
+    TurnType.RIGHT,   # turnType
+    90,   # angle
+    RotationUnits.DEG,   # rotationUnits
+    25,   # velocity
+    VelocityUnits.PCT,   # velocityUnits
+    True   # waitForCompletion
+)
