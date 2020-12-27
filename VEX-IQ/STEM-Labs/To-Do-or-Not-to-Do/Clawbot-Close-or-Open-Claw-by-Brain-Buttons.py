514db9b204eba022983b31c7c670ccd721b76ff4
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-26 16:18:25 -0800

    update VEX-IQ Clawbot-Close-or-Open-Claw-by-Brain-Buttons

--- VEX-IQ/STEM-Labs/To-Do-or-Not-to-Do/Clawbot-Close-or-Open-Claw-by-Brain-Buttons.py
+++ VEX-IQ/STEM-Labs/To-Do-or-Not-to-Do/Clawbot-Close-or-Open-Claw-by-Brain-Buttons.py
@@ -1,74 +1,15 @@
-from drivetrain import Drivetrain
-from vexiq import \
-    Bumper, \
-    ColorSensor, \
-    DistanceSensor, \
-    Gyro, \
-    Motor, \
-    TouchLed, \
-    UNIT_CM, \
-    is_up_button_pressed, is_down_button_pressed
+from vexiq import Motor, is_up_button_pressed, is_down_button_pressed
 
 
 class Clawbot:
-    def __init__(
-            self,
-            left_motor_port=1, right_motor_port=6,
-            wheel_travel_mm=200, track_mm=176,
-            touch_led_port=2,
-            color_sensor_port=3,
-            gyro_sensor_port=4,
-            distance_sensor_port=7,
-            bumper_switch_port=8,
-            arm_motor_port=10,
-            claw_motor_port=11):
-        self.drivetrain = \
-            Drivetrain(
-                Motor(
-                    left_motor_port,   # port
-                    False   # switch_polarity
-                ),   # left_motor
-                Motor(
-                    right_motor_port,   # port
-                    True   # switch_polarity
-                ),   # right_motor
-                wheel_travel_mm,   # wheel_travel_mm
-                track_mm   # track_mm
-            )
+    CLAW_MOTOR_PORT = 11
+    CLAW_MOTOR_REVERSE_POLARITY = False
 
-        self.touch_led = TouchLed(touch_led_port)
-
-        self.color_sensor = \
-            ColorSensor(
-                color_sensor_port,   # index
-                False,   # is_grayscale
-                700   # proximity
-            )
-
-        self.gyro_sensor = \
-            Gyro(
-                gyro_sensor_port,   # index
-                True   # calibrate
-            )
-
-        self.distance_sensor = \
-            DistanceSensor(
-                distance_sensor_port,   # port
-                UNIT_CM   # unit
-            )
-
-        self.bumper_switch = Bumper(bumper_switch_port)
-
-        self.arm_motor = \
-            Motor(
-                arm_motor_port,   # index
-                False   # reverse
-            )
-
+    def __init__(self):
         self.claw_motor = \
             Motor(
-                claw_motor_port,   # index
-                False   # reverse
+                self.CLAW_MOTOR_PORT,   # index
+                self.CLAW_MOTOR_REVERSE_POLARITY   # reverse
             )
 
 
