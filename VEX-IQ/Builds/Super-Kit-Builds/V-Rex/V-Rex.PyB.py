aa2fd76ed1e2e2b95be4c46698b84ffdf6246bcf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-30 21:19:40 -0800

    add VEX-IQ V-Rex programs

---
+++ VEX-IQ/Builds/Super-Kit-Builds/V-Rex/V-Rex.PyB.py
@@ -0,0 +1,34 @@
+from vex import Controller, DirectionType, Motor, Ports, VelocityUnits
+
+
+class VRex:
+    MOTOR_PORT = Ports.PORT7
+
+    CONTROLLER_DEADBAND = 3   # seconds
+
+    def __init__(self):
+        self.motor = \
+            Motor(
+                self.MOTOR_PORT,   # index
+                False   # reverse
+            )
+
+        self.controller = Controller()
+        self.controller.set_deadband(self.CONTROLLER_DEADBAND)
+
+    def drive_once_by_controller(self):
+        self.motor.spin(
+            DirectionType.FWD,   # dir
+            self.controller.axisA.position(),   # velocity
+            VelocityUnits.PCT   # velocityUnit
+        )
+
+    def keep_driving_by_controller(self):
+        while True:
+            self.drive_once_by_controller()
+
+
+V_REX = VRex()
+
+
+V_REX.keep_driving_by_controller()
---
+++ VEX-IQ/Builds/Super-Kit-Builds/V-Rex/V-Rex.py
@@ -0,0 +1,34 @@
+from vexiq import Joystick, Motor
+
+
+class VRex:
+    MOTOR_PORT = 7
+
+    CONTROLLER_DEADBAND = 3   # seconds
+
+    def __init__(self):
+        self.motor = \
+            Motor(
+                self.MOTOR_PORT,   # port
+                False   # switch_polarity
+            )
+
+        self.controller = Joystick()
+        self.controller.set_deadband(self.CONTROLLER_DEADBAND)
+
+    def drive_once_by_controller(self):
+        self.motor.run(
+            self.controller.axisA(),   # power
+            None,   # distance
+            False   # hold
+        )
+
+    def keep_driving_by_controller(self):
+        while True:
+            self.drive_once_by_controller()
+
+
+V_REX = VRex()
+
+
+V_REX.keep_driving_by_controller()
