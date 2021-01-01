4be63de29c6eaa4b17b74a3909d4db4c3c569f9b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-12-31 22:44:09 -0800

    add VEX-IQ Allie program

---
+++ VEX-IQ/Builds/Super-Kit-Builds/Allie/Allie.py
@@ -0,0 +1,118 @@
+from vexiq import Joystick, Motor
+
+
+class Allie:
+    FRONT_LEFT_MOTOR_PORT = 1
+    FRONT_LEFT_MOTOR_REVERSE_POLARITY = False
+
+    FRONT_RIGHT_MOTOR_PORT = 6
+    FRONT_RIGHT_MOTOR_REVERSE_POLARITY = True
+
+    REAR_LEFT_MOTOR_PORT = 7
+    REAR_LEFT_MOTOR_REVERSE_POLARITY = False
+
+    REAR_RIGHT_MOTOR_PORT = 12
+    REAR_RIGHT_MOTOR_REVERSE_POLARITY = True
+
+    MOTOR_TIMEOUT_SECS = 1
+
+    CONTROLLER_DEADBAND = 3   # seconds
+
+    def __init__(self):
+        self.front_left_motor = \
+            Motor(
+                self.FRONT_LEFT_MOTOR_PORT,   # port
+                self.FRONT_LEFT_MOTOR_REVERSE_POLARITY   # switch_polarity
+            )
+        self.front_left_motor.stall_timeout = self.MOTOR_TIMEOUT_SECS
+
+        self.front_right_motor = \
+            Motor(
+                self.FRONT_RIGHT_MOTOR_PORT,   # port
+                True   # switch_polarity
+            )
+        self.front_right_motor.stall_timeout = self.MOTOR_TIMEOUT_SECS
+
+        self.rear_left_motor = \
+            Motor(
+                self.REAR_LEFT_MOTOR_PORT,   # port
+                self.REAR_LEFT_MOTOR_REVERSE_POLARITY   # switch_polarity
+            )
+        self.rear_left_motor.stall_timeout = self.MOTOR_TIMEOUT_SECS
+
+        self.rear_right_motor = \
+            Motor(
+                self.REAR_RIGHT_MOTOR_PORT,   # port
+                self.REAR_RIGHT_MOTOR_REVERSE_POLARITY   # switch_polarity
+            )
+        self.rear_right_motor.stall_timeout = self.MOTOR_TIMEOUT_SECS
+
+        self.controller = Joystick()
+        self.controller.set_deadband(self.CONTROLLER_DEADBAND)
+
+    def drive_once_by_controller(self):
+        if self.controller.bLdown():
+            self.front_left_motor.run_until(
+                100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bLup():
+            self.front_left_motor.run_until(
+                -100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bRdown():
+            self.front_right_motor.run_until(
+                100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bRup():
+            self.front_right_motor.run_until(
+                -100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bEup():
+            self.rear_left_motor.run_until(
+                100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bEdown():
+            self.rear_left_motor.run_until(
+                -100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bFup():
+            self.rear_right_motor.run_until(
+                100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+        elif self.controller.bFdown():
+            self.rear_right_motor.run_until(
+                -100,   # power
+                30,   # distance
+                True   # hold
+            )
+
+    def keep_driving_by_controller(self):
+        while True:
+            self.drive_once_by_controller()
+
+
+ALLIE = Allie()
+
+
+ALLIE.keep_driving_by_controller()
