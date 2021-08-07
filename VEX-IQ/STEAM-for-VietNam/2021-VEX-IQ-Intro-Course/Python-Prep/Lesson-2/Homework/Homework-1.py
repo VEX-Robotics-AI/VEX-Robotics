c89b799eea1e697b1e83177a2f71150621f32fda
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 00:23:58 -0700

    add Python-Prep-Lesson-2 Homework files

---
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-1.py
@@ -0,0 +1,3 @@
+"""
+Tính trung bình cộng các số lẻ từ 1 đến 50
+"""
---
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-2.py
@@ -0,0 +1,5 @@
+"""
+Viết một hàm
+- Nhận vào tham số là 1 danh sách các số nguyên
+- Trả về tổng các số lẻ trong danh sách đó
+"""
