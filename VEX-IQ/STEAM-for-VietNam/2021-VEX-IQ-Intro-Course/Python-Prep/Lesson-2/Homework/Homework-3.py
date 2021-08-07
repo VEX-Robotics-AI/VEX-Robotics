7b8041ac1f133c4bf16046011ead204ca7d595ca
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 16:22:49 -0700

    add HW 3

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-3.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-3.py
@@ -3,11 +3,13 @@ Viết một hàm
 - Nhận vào tham số là 1 danh sách các chuỗi
 - Trả về chuỗi có độ dài lớn nhất
 """
-numbers = [100, 90, 200]
 
-for number in numbers:
-    the_largest_number = 0
-    if number > the_largest_number:
-        the_largest_number = number
 
-print(f"The largest number is {the_largest_number}.")
+strs = ["90", "name", "*"]
+result = None
+
+for s in strs:
+    if (result is None) or (len(s) > len(result)):
+        result = s
+
+print(f"The longest string is '{result}'.")
