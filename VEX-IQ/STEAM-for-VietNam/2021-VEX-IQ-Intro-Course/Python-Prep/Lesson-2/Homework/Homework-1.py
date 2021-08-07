0a78b574b60e0e383a9118d5853476353498b99c
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 16:15:56 -0700

    add HW 1 and 2

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-1.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-1.py
@@ -3,12 +3,16 @@ Tính trung bình cộng các số lẻ từ 1 đến 50
 """
 
 
+def is_odd(x: int) -> bool:
+    return x % 2 != 0
+
+
 i = 0
 total_of_odds = 0
 n_odds = 0
 while i < 50:
     i += 1
-    if i % 2 != 0:
+    if is_odd(i):
         total_of_odds += i
         n_odds += 1
 
--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-2.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-2.py
@@ -3,14 +3,17 @@ Viết một hàm
 - Nhận vào tham số là 1 danh sách các số nguyên
 - Trả về tổng các số lẻ trong danh sách đó
 """
-numbers = [125, 256, 58]
+
+
+def is_odd(x: int) -> bool:
+    return x % 2 != 0
+
+
+numbers = [-67]  # [125, 259, 59, 90, 80, 70, -90, -70, 33, -33, 55, 90]
 result = 0
 
 for number in numbers:
-    odd_or_even = int(number / 2)
-    
-    odd_or_even = 0
-    # tens, ones = number.split()
+    if is_odd(number):
+        result += number
 
-
-
+print(result)
