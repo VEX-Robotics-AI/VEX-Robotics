4f776c963b5c061b87b89520dfc5ae375078f3bf
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 00:59:24 -0700

    add Python-Prep-Lesson-4 Scope-in-Python

---
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-4/Scope-in-Python.py
@@ -0,0 +1,19 @@
+"""
+https://bit.ly/S4V_CS201_Scope_Python
+https://www.robotmesh.com/studio/60e48f6ec4b0580fb15cca04
+"""
+
+
+# --- Giới Thiệu Scope Trong Python ---
+def set_name():
+    name = "CS201"
+    print(name)
+
+
+# def get_name():
+#     print(name)
+
+
+# get_name()
+set_name()
+# --------------------------
