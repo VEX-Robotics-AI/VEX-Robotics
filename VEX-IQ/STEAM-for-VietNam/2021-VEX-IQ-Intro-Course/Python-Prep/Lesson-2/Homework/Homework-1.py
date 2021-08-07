fa282c48e90fb008c6d856e1e62dddd67f55d133
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 16:05:58 -0700

    add HW 1

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-1.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-1.py
@@ -1,9 +1,15 @@
 """
 Tính trung bình cộng các số lẻ từ 1 đến 50
 """
+
+
 i = 0
-total = 0
+total_of_odds = 0
+n_odds = 0
 while i < 50:
     i += 1
-    total += i
-print(total / 50)
+    if i % 2 != 0:
+        total_of_odds += i
+        n_odds += 1
+
+print(total_of_odds / n_odds)
