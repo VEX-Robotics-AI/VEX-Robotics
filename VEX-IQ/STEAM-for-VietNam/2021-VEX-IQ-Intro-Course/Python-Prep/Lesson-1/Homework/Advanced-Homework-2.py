ca267398db67eddbf124dc61cef1a51e25f5effa
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 16:00:32 -0700

    add Advanced HW 2

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Advanced-Homework-2.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Advanced-Homework-2.py
@@ -3,3 +3,21 @@
 Ví dụ: danh_sach = [1, 5, 2, 2, 9, 1, 4, 9, 2, 9, 2]
 Kết quả xuất ra là: Có 3 phần tử có giá trị là 9
 """
+
+
+numbers = [1, 5, 2, 2, 9, 1, 4, 9, 2, 9, 2]
+
+# Find the largest number
+the_largest_number = None
+for number in numbers:
+    if (the_largest_number is None) or (number > the_largest_number):
+        the_largest_number = number
+
+# Find the count of the largest number
+count = 0
+for number in numbers:
+    if number == the_largest_number:
+        count += 1
+
+
+print(f"Có {count} phần tử có giá trị là {the_largest_number}.")
