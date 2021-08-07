a3ff14435bbd42a63301b4e9dfd6f6c86b4ad54f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 15:53:25 -0700

    add Advanced HW 1

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Advanced-Homework-1.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Advanced-Homework-1.py
@@ -2,3 +2,13 @@
 Tìm và xuất phần tử có giá trị lớn nhất trong danh sách.
 Ví dụ: danh_sach = [1, 5, 2, 3, 13, 2]. Kết quả xuất ra là: 13
 """
+
+
+numbers = [1, 5, 2, 3, 13, 2]
+the_largest_number = None
+
+for number in numbers:
+    if (the_largest_number is None) or (number > the_largest_number):
+        the_largest_number = number
+
+print(f"The largest number is {the_largest_number}.")
