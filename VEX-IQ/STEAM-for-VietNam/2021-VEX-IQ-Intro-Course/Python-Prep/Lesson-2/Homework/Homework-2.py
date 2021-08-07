265d6ec9e35916f9251128f64a76b2a8d9279d45
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 15:29:20 -0700

    add HW 2

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-2.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-2.py
@@ -3,3 +3,14 @@ Viết một hàm
 - Nhận vào tham số là 1 danh sách các số nguyên
 - Trả về tổng các số lẻ trong danh sách đó
 """
+numbers = [125, 256, 58]
+result = 0
+
+for number in numbers:
+    odd_or_even = int(number / 2)
+    
+    odd_or_even = 0
+    # tens, ones = number.split()
+
+
+
--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-3.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/Homework/Homework-3.py
@@ -3,3 +3,11 @@ Viết một hàm
 - Nhận vào tham số là 1 danh sách các chuỗi
 - Trả về chuỗi có độ dài lớn nhất
 """
+numbers = [100, 90, 200]
+
+for number in numbers:
+    the_largest_number = 0
+    if number > the_largest_number:
+        the_largest_number = number
+
+print(f"The largest number is {the_largest_number}.")
