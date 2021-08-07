71c9f9225ab6787f3b3260f75e0a496d65e645c1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 00:04:25 -0700

    add Python-Prep-Lesson-2 For-Loop-2

---
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/For-Loop-2.py
@@ -0,0 +1,59 @@
+"""
+http://bit.ly/S4V_CS201_For_Loop2
+https://www.robotmesh.com/studio/60bd46f85ba6e20556d7ece8
+"""
+
+
+# Tính tổng các số từ 0 tới 9
+total = 0
+for value in range(10):
+    total += value   # total = total + value
+# print(total)
+
+# ===============================================
+# Thực tế thì range có cấu trúc gần giống như 1 danh sách:
+# print(range(10))
+
+
+# ===============================================
+# Ra lệnh cho robot di chuyển theo 1 hình vuông cạnh 100cm
+# print('Robot đi thẳng 100cm')
+# print('Robot rẽ phải 90 độ')
+
+# print('Robot đi thẳng 100cm')
+# print('Robot rẽ phải 90 độ')
+
+# print('Robot đi thẳng 100cm')
+# print('Robot rẽ phải 90 độ')
+
+# print('Robot đi thẳng 100cm')
+# print('Robot rẽ phải 90 độ')
+
+# for i in range(4):
+#     print('Robot đi thẳng 100cm')
+#     print('Robot rẽ phải 90 độ')
+#     print('')
+
+
+# ===============================================
+# In ra 3 dòng, mỗi dòng 10 dấu *
+# for i in range(3):
+#     line = ''
+#     for j in range(10):
+#         line = line + '*'
+#     print(line)
+
+
+# ===============================================
+# Tính tổng các số từ 2 tới 9
+# total = 0
+# for value in range(2, 10):
+#     total = total + value
+# print(total)
+
+# total = 0
+# for value in range(2,10,2):
+#     total = total + value
+# print(total)
+
+print(range(5, 45, 5))
