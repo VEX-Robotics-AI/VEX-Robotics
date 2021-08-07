9ce1ea49341a86a93867a7e38d1e6eae542deb34
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 00:17:12 -0700

    add Python-Prep-Lesson-2 OOP

---
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-2/OOP.py
@@ -0,0 +1,27 @@
+"""
+http://bit.ly/S4V_CS201_OOP
+https://www.robotmesh.com/studio/60bd58484271ec49ce231d80
+"""
+
+
+# ========================
+# List
+
+gio_hang = ['cam', 'tao', 'quyt', 'nho']   # doi tuong ten la gio_hang
+
+gio_2 = []   # hay gio_2 = List() --> tương tự như ki = Dog("ki")
+
+# Phuong thuc
+# List.append(item) (2 lan)
+gio_2.append('vai')
+gio_2.append('nhan')
+print(gio_2)
+
+print('----')
+# List.pop()
+lay_ra = gio_2.pop()
+print(lay_ra)
+print(gio_2)
+
+gio_2.pop()
+print(gio_2)
