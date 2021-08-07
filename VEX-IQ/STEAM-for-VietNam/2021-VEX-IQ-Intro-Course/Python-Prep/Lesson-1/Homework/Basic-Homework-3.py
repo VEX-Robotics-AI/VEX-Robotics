2279f04ae6b8d2f21756c316c110acba281101a5
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-07 16:37:03 -0700

    add Basic HW 3

--- VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Basic-Homework-3.py
+++ VEX-IQ/STEAM-for-VietNam/2021-VEX-IQ-Intro-Course/Python-Prep/Lesson-1/Homework/Basic-Homework-3.py
@@ -3,10 +3,15 @@ Viết chương trình tính tổng của các số trong một danh sách
 """
 
 
+def find_sum(list_of_numbers: list[int]) -> int:
+    total = 0
+
+    for number in list_of_numbers:
+        total += number
+
+    return total
+
+
 numbers = [1, 2, 7]
-total = 0
 
-for number in numbers:
-    total += number
-
-print(total)
+print(find_sum(numbers))
