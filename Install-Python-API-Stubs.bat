938b3e3c9a379fe8e0959e7f26d34c61610f1181
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-08-09 22:15:27 -0700

    update Install-Python-API-Stubs script

--- Install-Python-API-Stubs.bat
+++ Install-Python-API-Stubs.bat
@@ -1,2 +1,2 @@
-python3 -m pip install --no-cache-dir --upgrade --user ^
+python -m pip install --no-cache-dir --upgrade --user ^
     https://GitHub.com/STEAMforVietnam/RobotMesh-VEXIQ-PyB-API-Stubs/archive/main.zip
