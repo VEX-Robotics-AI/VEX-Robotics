ebd01390589e6ece3d65c813117dc6ffe3b7dda7
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-12 23:30:13 -0700

    update VEXcode VR Grid-Map Basketball-Drills Python programs

--- VEXcode-VR/Activities/Grid-Map/Basketball-Drills/Basketball-Drills-2.py
+++ VEXcode-VR/Activities/Grid-Map/Basketball-Drills/Basketball-Drills-2.py
@@ -1,17 +1,26 @@
+from vexcode import (
+    DEGREES, FORWARD, RIGHT, MM,
+    drivetrain, vr_thread
+)
+
+
 def when_started():
     # Drive forward and back 1 grid block
     drivetrain.drive_for(FORWARD, 200, MM)
     drivetrain.turn_for(RIGHT, 180, DEGREES)
     drivetrain.drive_for(FORWARD, 200, MM)
     drivetrain.turn_for(RIGHT, 180, DEGREES)
+
     # Drive forward and back 2 grid blocks
     drivetrain.drive_for(FORWARD, 400, MM)
     drivetrain.turn_for(RIGHT, 180, DEGREES)
     drivetrain.drive_for(FORWARD, 400, MM)
     drivetrain.turn_for(RIGHT, 180, DEGREES)
+
     # Drive forward and back 4 grid blocks
     drivetrain.drive_for(FORWARD, 800, MM)
     drivetrain.turn_for(RIGHT, 180, DEGREES)
     drivetrain.drive_for(FORWARD, 800, MM)
+
 
 vr_thread(when_started())
--- VEXcode-VR/Activities/Grid-Map/Basketball-Drills/Basketball-Drills-3.py
+++ VEXcode-VR/Activities/Grid-Map/Basketball-Drills/Basketball-Drills-3.py
@@ -1,12 +1,24 @@
+from vexcode import (
+    FORWARD, REVERSE, MM, MSEC, wait,
+    drivetrain, vr_thread
+)
+
+
 gridblocks = 0
+
 
 def when_started():
     global gridblocks
+
     gridblocks = 1
-    for repeat_count in range(8):
+
+    for _ in range(8):
         drivetrain.drive_for(FORWARD, (200 * gridblocks), MM)
         drivetrain.drive_for(REVERSE, (200 * gridblocks), MM)
+
         gridblocks = gridblocks + 1
+
         wait(5, MSEC)
+
 
 vr_thread(when_started())
