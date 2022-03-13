d6a64553d168f8d4016c13b859909552361577db
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-12 19:53:15 -0800

    add Community.orgs.STEAMforVietNam courses/2022-VEX-IQ-Intro-Course/Lesson-2/homework/Programming-Advanced

--- Community/orgs/STEAMforVietNam/courses/2022-VEX-IQ-Intro-Course/Lesson-2/homework/Programming-Advanced.py
+++ Community/orgs/STEAMforVietNam/courses/2022-VEX-IQ-Intro-Course/Lesson-2/homework/Programming-Advanced.py
@@ -6,3 +6,27 @@ Tốc độ di chuyển và xoay của robot đều là 50%.
 Hình dưới đây mô tả vị trí khi xuất phát & kết thúc của robot,
 cũng như đường đi được yêu cầu:
 """
+
+
+# IMPORT CÁC ĐỐI TƯỢNG TỪ THƯ VIỆN
+# ================================
+
+from vex import (
+    Motor, Ports,
+    LEFT, RIGHT, FORWARD, REVERSE, DEGREES, PERCENT, DistanceUnits
+)
+from drivetrain import Drivetrain
+
+
+# KHỞI TẠO CÁC BỘ PHẬN ROBOT
+# ==========================
+
+# khởi tạo các motor
+motor_left = Motor(Ports.PORT1, True)
+motor_right = Motor(Ports.PORT2)   # reverse=True: đảo ngược chiều xoay
+
+# khởi tạo drivetrain với 2 motor, mỗi motor gắn một bánh xe
+# chu vi 200mm và khoảng cách giữa 2 bánh xe 2 bên là 176mm
+dt = Drivetrain(motor_left, motor_right, 200, 176)
+
+# Học sinh lập trình theo yêu cầu đề bài ở phần dưới đây:
