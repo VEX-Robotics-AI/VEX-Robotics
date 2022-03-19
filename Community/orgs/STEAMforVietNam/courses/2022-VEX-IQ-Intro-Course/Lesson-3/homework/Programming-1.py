616955a5d7b4ef84c93bda4fb8ccf65876d1370f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-03-19 00:36:25 -0700

    update Community.orgs.STEAMforVietNam courses/2022-VEX-IQ-Intro-Course/Lesson-3/homework/Programming-1

--- Community/orgs/STEAMforVietNam/courses/2022-VEX-IQ-Intro-Course/Lesson-3/homework/Programming-1.py
+++ Community/orgs/STEAMforVietNam/courses/2022-VEX-IQ-Intro-Course/Lesson-3/homework/Programming-1.py
@@ -4,7 +4,11 @@ Lập trình cho đèn TouchLED nhấp nháy liên tục với chu kỳ
 """
 
 
+# NHẬP CÁC ĐỐI TƯỢNG TỪ THƯ VIỆN
+# ==============================
+
 from vex import Ports, Touchled, ColorHue
+import sys
 
 
 # KHỞI TẠO CÁC BỘ PHẬN ROBOT
@@ -14,11 +18,28 @@ from vex import Ports, Touchled, ColorHue
 touch_led = Touchled(Ports.PORT10)
 
 
-# Học sinh sẽ phải lập trình cho hàm operate() sau đây để điều khiển TouchLED thực hiện yêu cầu đề bài
+# ĐỊNH NGHĨA CÁC HÀM
+# ==================
+
+# Học sinh sẽ phải lập trình cho hàm operate() sau đây
+# để điều khiển TouchLED thực hiện yêu cầu đề bài
 def operate():
-    ...
+    # Bật đèn LED màu đỏ
+    touch_led.on_hue(ColorHue.RED)
+    # Đợi trong 3 giây
+    sys.sleep(3)
+
+    # Bật đèn LED màu vàng
+    touch_led.on_hue(ColorHue.YELLOW)
+    # Đợi trong 1 giây
+    sys.sleep(1)
+
+    # Tắt đèn LED
+    touch_led.off()
 
 
-# Chương trình chính
+# CHƯƠNG TRÌNH CHÍNH
+# ==================
+
 while True:
     operate()
