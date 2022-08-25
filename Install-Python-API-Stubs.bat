23874a210e4bc7b84808895bbf0b1594266e80bf
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-08-25 13:18:10 -0700

    update install-VEX-Py scripts

--- Install-Python-API-Stubs.bat
+++
@@ -1,4 +0,0 @@
-python -m pip install --upgrade PIP SetUpTools
-
-python -m pip install --no-cache-dir --upgrade --user ^
-    https://GitHub.com/VEX-Robotics-AI/RobotMesh-VEX-PyB-API-Stubs/archive/main.zip
---
+++ install-VEX-Py.bat
@@ -0,0 +1,4 @@
+python -m pip install --upgrade PEP517 PIP SetUpTools Wheel
+
+python -m pip install --upgrade --user ^
+    https://GitHub.com/VEX-Robotics-AI/VEX-Py/archive/main.zip
