0f19fe7ec7488539daa25e76744c3cadaea3bc88
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2022-11-20 13:10:10 -0800

    add Computing-Platforms/VEX-IQ/Tests/Test-Imports.Gen2.VEXcode program

---
+++ Computing-Platforms/VEX-IQ/Tests/Test-Imports.Gen2.VEXcode.py
@@ -0,0 +1,33 @@
+"""drive.google.com/file/d/10c7uscsGrdhtjqn0OzDdRyv48htE5BuY"""
+
+
+# flake8: noqa
+# pylint: disable=unused-import
+
+
+from vex import (
+    Brain, SoundType,
+    Ports,
+    Inertial,
+    AxisType,
+    OrientationType,
+    Bumper,
+    ColorSensor,
+    Optical, LedStateType, GestureType,
+    Distance, ObjectSizeType,
+    Sonar,
+    Controller,
+    Gyro, GyroCalibrationType,
+    Motor,
+    BrakeType, COAST, BRAKE, HOLD,
+    DirectionType, FORWARD, REVERSE,
+    TorqueUnits,
+    TurnType, LEFT, RIGHT,
+    VelocityUnits,
+    Touchled, FadeType,
+    TimeUnits, SECONDS, MSEC, wait,
+    DistanceUnits, MM, INCHES,
+    CurrentUnits,
+    PERCENT,
+    RotationUnits, DEGREES, TURNS,
+)
