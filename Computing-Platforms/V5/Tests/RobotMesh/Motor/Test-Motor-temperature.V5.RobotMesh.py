78d13b3679b18d0061e46601f6c011d8ae16cfb3
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-07-03 15:50:52 -0700

    update Computing-Platforms/V5/Tests/RobotMesh/Motor/Test-Motor-temperature.V5.RobotMesh program

--- Computing-Platforms/V5/Tests/RobotMesh/Motor/Test-Motor-temperature.V5.RobotMesh.py
+++ Computing-Platforms/V5/Tests/RobotMesh/Motor/Test-Motor-temperature.V5.RobotMesh.py
@@ -14,6 +14,9 @@ brain = Brain()
 motor = Motor(Ports.PORT1)
 
 
+motor_temperature = motor.temperature()
+# assert isinstance(motor_temperature, float)
+
 motor_temperature_c = motor.temperature(TemperatureUnits.CELSIUS)
 # assert isinstance(motor_temperature_c, float)
 
@@ -24,9 +27,10 @@ motor_temperature_pct = motor.temperature(TemperatureUnits.PCT)
 # assert isinstance(motor_temperature_pct, float)
 
 
-brain.screen.print_line(1, motor_temperature_c)
-brain.screen.print_line(2, motor_temperature_f)
-brain.screen.print_line(3, motor_temperature_pct)
+brain.screen.print_line(1, motor_temperature)
+brain.screen.print_line(2, motor_temperature_c)
+brain.screen.print_line(3, motor_temperature_f)
+brain.screen.print_line(4, motor_temperature_pct)
 
 
 wait(3, SECONDS)
